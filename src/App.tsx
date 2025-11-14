daaeaa38d6404eaa1e8baf56794f949bc49a6583
VladimirKoshovec <koshavladmir@gmail.com>
2025-11-14 17:07:47 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Boardform from './conponents/boardform/Boardform';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Mokaps</h1>
+      <Boardform/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conponents/boardform/Boardform.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import css from './boardform.module.scss';
+
+ import {
+   Formik,
+   Form,
+   Field
+ } from 'formik';
+
+ interface MyFormValues {
+   firstName: string;
+ }
+
+const Boardform = () => {
+   const initialValues: MyFormValues = { firstName: '' };
+
+   return (
+
+     <div className={ css.boardFrame }>
+       <Formik
+
+         initialValues={initialValues}
+
+         onSubmit={(values, actions) => {
+
+           console.log({ values, actions });
+
+           alert(JSON.stringify(values, null, 2));
+
+           actions.setSubmitting(false);
+
+         }}
+
+       >
+
+         <Form className={ css.boardForm }>
+
+           <label htmlFor="firstName"></label>
+
+           <Field id="firstName" name="firstName" placeholder="Board Name" className={ css.formInput }/>
+
+           <button type="submit" className={ css.formButton }>Submit</button>
+
+         </Form>
+
+       </Formik>
+
+     </div>
+
+   );
+
+ };
+
+export default Boardform;
